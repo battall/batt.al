a594a7e6de1fd1a894bf3d2021cc61f403bcb10d
“battall” <battal51280@gmail.com>
2024-09-20 17:08:54 +0300

    feat: ContentInfo component

---
+++ src/routes/photos/ContentInfo.svelte
@@ -0,0 +1,30 @@
+<div class="bg-[#ffffff] *:px-4">
+  <input class="h-12 w-full border-b border-[#ebebec] bg-[#ffffff] placeholder-[#c5c5c7]" placeholder="Add a Caption" />
+  <div class=" flex flex-col gap-4 bg-[#f7f7fa]">
+    <div class="flex justify-between pt-5">
+      <div class="text-start">
+        <p class="text-[#000000]">Sunday &bull; 18 Aug 2024 &bull; 15:09</p>
+        <p class="text-[#87878c]">IMG_9528</p>
+      </div>
+      <p class="text-primary">Adjust</p>
+    </div>
+    <div class="overflow-hidden rounded-lg *:p-1.5">
+      <div class="flex justify-between bg-[#dedee3]">
+        <span class="text-[#000000]">Apple Iphone XS</span>
+        <div><span class="rounded bg-[#afafb4] px-1 py-1.5 text-xs text-[#ffffff]">JPEG</span></div>
+      </div>
+      <div class="border-y border-[#d3d3d7] bg-[#ececf0] text-start text-[#828288]">
+        <p>Wide Camera -- 26 mm &fnof;1.8</p>
+        <p>12 MP &bull; 4032 x 3024 &bull; 3,8 MB</p>
+      </div>
+      <div class="grid grid-cols-5 divide-x bg-[#ececf0] text-sm text-[#828288] *:border-[#d3d3d7]">
+        <span class="text-start">ISO 800</span>
+        <span>26 mm</span>
+        <span>0 ev</span>
+        <span>&fnof;1.8</span>
+        <span class="text-end">1/25 s</span>
+      </div>
+    </div>
+    <button class="w-full rounded-lg bg-[#ececf0] py-4 text-center text-primary"> Add a location... </button>
+  </div>
+</div>
