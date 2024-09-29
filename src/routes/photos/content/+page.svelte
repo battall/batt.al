653af41e3abd14d29ed19d86aa48e62a7d8f80aa
“battall” <battal51280@gmail.com>
2024-09-29 13:35:40 +0300

    chore: dummy pages to be able to route

---
+++ src/routes/photos/content/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { afterNavigate, beforeNavigate, onNavigate } from "$app/navigation";
+
+  beforeNavigate((navigation) => {
+    console.log("[NAVIGATE][B4]", navigation);
+
+    // in befores, get the current content image rect
+  });
+  afterNavigate((navigation) => {
+    console.log("[NAVIGATE][AT]", navigation);
+
+    // in here, get the current saved rect from beforerect
+    // get the newer rect
+    // animate, based on which page were
+  });
+  onNavigate((navigation) => {
+    console.log("[NAVIGATE][ON]", navigation);
+  });
+</script>
+
+<div>
+  <a href="/photos">go back to photos</a>
+</div>
